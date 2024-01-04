5287a4b334e162e8564edb32dc23c4e20ca7515a
Therese <therese.holen22@gmail.com>
2024-01-04 11:15:50 +0100

    Added mysql, cors, crypto-js, dotenv.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,37 @@
+require('dotenv').config();
+
 var express = require('express');
+var router = express.Router();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var CryptoJS = require("crypto-js");
+
+const mysql = require('mysql2');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//import 'dotenv/config'
+
+
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,101 @@
 var express = require('express');
 var router = express.Router();
+const CryptoJS = require('crypto-js');
+const mysql = require('mysql2');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Välkommen till users');
+//
+// Registrera ny användare
+//
+
+router.post('/', function(req,res) {
+  let newUser = req.body; 
+  console.log(newUser);
+
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    let sql = `SELECT userEmail FROM users WHERE userEmail = ${mysql.escape(newEmail)}`;
+
+    req.app.locals.con.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("result", result);
+      if (result.length == 0) {
+        createNewUser(req, res);
+        return;
+      }
+      res.send(401);
+    })
+  })
 });
 
+function createNewUser (req, res) {
+  let newUser = req.body; 
+  console.log(newUser);
+
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    let passwordToSave = CryptoJS.SHA3(req.body.newPassword).toString()
+
+    let sql = `INSERT INTO users (userEmail, userPassword, userFirstname, userLastname) VALUES (${mysql.escape(newUser.newEmail)}, ${mysql.escape(passwordToSave)}, ${mysql.escape(newUser.newFirstname)}, ${mysql.escape(newUser.newL)})`;
+  
+    req.app.locals.con.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("result", result) 
+      res.send(result);
+    })
+  })
+};
+
+
+//
+// Logga in användare 
+//
+
+router.post('/login', function(req,res) {
+  let { userEmail, userPassword } = req.body ;
+
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    let passwordToCheck = CryptoJS.SHA3(userPassword).toString()
+    console.log(passwordToCheck);
+
+    let sql = `SELECT userEmail, userPassword, userId FROM users WHERE userEmail = ${mysql.escape(userEmail)}`;
+    console.log(sql);
+
+    req.app.locals.con.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log(result);
+
+      let userToken = (CryptoJS.SHA3(result[0].userID + process.env.TOKEN).toString())
+      if(passwordToCheck === result[0].userPassword) {
+        res.json({userId:result[0].userId, token:userToken})
+        return;
+      }
+      res.status(401).json("Incurrect password or email")
+    })
+  })
+})
+
+
 module.exports = router;
+
+
+/* GET users listing. 
+router.get('/', function(req, res, next) {
+  res.send('Välkommen till users');
+});*/
+
