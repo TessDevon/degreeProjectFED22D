05a7e4c2a7243a0ea5c37af5c214eaf5243186ef
Therese <therese.holen22@gmail.com>
2024-01-05 13:04:37 +0100

    Added form to login. TODO regexp to validate code?

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+import { registerPerson } from '../models/PersonClass';
 
 
 const customStyles = {
@@ -13,27 +14,57 @@ const customStyles = {
     },
   };
 
-  /*
-  Settings for other modals like chatt
-    //Två nästkommande rader för att stänga rutan om man trycker utanför.
-    shouldCloseOnEsc={true}
-    onRequestClose={() => setShowLoginModal(false)}
-    isOpen={modalIsOpen}
-    onAfterOpen={afterOpenModal}
-    style={customStyles}
-    contentLabel="Example Modal"*/
-
 export default function LoginModal() {
     Modal.setAppElement('#root');
     const [showLoginModal, setShowLoginModal] = useState(true)
+    
+
+    const [formData, setFormData] = useState<registerPerson>({
+        firstname: "",
+        lastname:"",
+        email: "",
+        password: "",
+        userImage: "",
+    });
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        
+        const name = e.target.name; 
+
+        if(e.target.type === "text") {
+            setFormData({...formData, [name]:e.target.value});
+        }
+        if(e.target.type === "number") {
+            setFormData({...formData, [name]:+e.target.value});
+        }
+    }
+    const handleSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        console.log(formData)
+        //setShowLoginModal(false)
+    }
+
 
     return (
-        <div className= "loginView">
+        <div className= 'signView'>
             <button onClick={() => setShowLoginModal(true)}>Öppna loggin</button>
             <Modal isOpen={showLoginModal} style={customStyles}>
                 <h1>Gå vidare för att ta del av vår unika minivärld!</h1>
                 <p> Våra miniatyrvärldar är en inspirationssida för alla som samlar, renoverar och har docksåp som hobby. Här kan man få inspiration genom inlägg från användarna men även en möjlighet att köpa miniatyrer av andra användare eller sälja det man inte längre behöver.</p>
-                <button onClick={() => setShowLoginModal(false)}>Logga in</button> 
+                <div className='loginView'>
+                    <form onSubmit={handleSubmit}>
+                        <input value={formData.firstname} type='text' onChange={handleChange} name='firstname'/>
+                        <input value={formData.lastname} type='text' onChange={handleChange} name='lastname'/>
+                        <input value={formData.email} type='text' onChange={handleChange} name='email'/>
+                        <input value={formData.password} type='text' onChange={handleChange} name='password'/>
+                        <input value={formData.userImage} type='img' onChange={handleChange} name='userImage'/>
+                        <button>Logga in</button> 
+                    </form>
+                    <p>{JSON.stringify(formData)}</p>
+                </div>
+                <div className='registerView'>
+
+                </div>
             </Modal>
             
             // Här ska läggas till loginfunktionen och om inloggning eller resistreing är okej med backend ska potalen stängas ner och sidan visas. 
---
+++ frontend/src/models/PersonClass.ts
@@ -0,0 +1,9 @@
+export class registerPerson {
+    constructor (
+        public firstname: string,
+        public lastname: string, 
+        public email: string,
+        public password: string,
+        public userImage: string,      
+    ) {}
+}
