eb7f2df5ad40c10b95b9cdb0d8dcedd0436714af
Therese <therese.holen22@gmail.com>
2024-01-08 13:13:15 +0100

    Save the user locally to remain logged in to the page.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const CryptoJS = require('crypto-js');
 const mysql = require('mysql2');
 
+
 //
 // Registrera ny användare
 //
@@ -15,7 +16,7 @@ router.post('/', function(req,res) {
     if (err) {
       console.log(err);
     }
-    let sql = `SELECT userEmail FROM users WHERE userEmail = ${mysql.escape(newEmail)}`;
+    let sql = `SELECT userEmail FROM users WHERE userEmail = ${mysql.escape(newUser.newEmail)}`;
 
     req.app.locals.con.query(sql, function(err, result) {
       if (err) {
@@ -51,6 +52,22 @@ function createNewUser (req, res) {
       console.log("result", result) 
       res.send(result);
     })
+
+    // Sparar användaren lokalt för att testas mot sidan när användaren hanterar sidan.
+    req.app.locals.con.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log(result);
+
+      let userToken = (CryptoJS.SHA3(result[0].userID + process.env.TOKEN).toString())
+      if(passwordToCheck === result[0].userPassword) {
+        res.json({userId:result[0].userId, token:userToken})
+        return;
+      }
+      res.status(401).json("Incurrect password or email")
+    })
   })
 };
 
@@ -58,6 +75,7 @@ function createNewUser (req, res) {
 //
 // Logga in användare 
 //
+
 
 router.post('/login', function(req,res) {
   let { userEmail, userPassword } = req.body ;
@@ -73,6 +91,7 @@ router.post('/login', function(req,res) {
     let sql = `SELECT userEmail, userPassword, userId FROM users WHERE userEmail = ${mysql.escape(userEmail)}`;
     console.log(sql);
 
+    // Sparar användaren lokalt för att testas mot sidan när användaren hanterar sidan. 
     req.app.locals.con.query(sql, function(err, result) {
       if (err) {
         console.log(err);
