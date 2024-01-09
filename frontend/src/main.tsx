14e1bc652a76c1363d355b30449bff7b6634ba43
Therese <therese.holen22@gmail.com>
2024-01-09 11:30:14 +0100

    Added i18next to the project.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './services/i18next.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ frontend/src/services/i18next.ts
@@ -0,0 +1,53 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+
+i18next
+.use(initReactI18next)
+.init({
+    compatibilityJSON: 'v3',
+    debug:true,
+    fallbackLng: 'sv',
+    interpolation: {escapeValue: false},
+    resources: { 
+        sv: {
+            translation: {
+                //header
+                appName: 'Våra minivärldar',
+
+                //Navigation
+                navigationInspirationText: 'Inspiration',
+                navigationBuySellText: 'Köp och sälj',
+                navigationAboutText: 'Om oss',
+                navigationLogoutText: 'Logga ut',
+                
+                //Loginview
+                loginMainheader: 'Gå vidare för att ta del av vår unika minivärld!',
+                loginMaintext: 'Våra miniatyrvärldar är en inspirationssida för alla som samlar, renoverar och har docksåp som hobby. Här kan man få inspiration genom inlägg från användarna men även en möjlighet att köpa miniatyrer av andra användare eller sälja det man inte längre behöver.',
+                loginInnerMainheader: 'Logga in',
+                loginFormEmailText: 'Epost',
+                loginFormPasswordText: 'Lösenord (minst 6 tecken, varav en stor bokstav och en siffra)',
+                loginFormBtnText: 'Logga in',
+                loginRegisterBtnText: 'Registrera dig som ny användare',
+
+                //Registerview
+                registerInnerMainheader: 'Registrera ny användare',
+                registerFirstnameText: 'Namn',
+                registerLastnameText: 'Efternamn',
+                registerUserImgText: 'Användarbild (filformatet jpg och png)',
+                registerRegistrationbtnText:'Registrera',
+
+                //loginErrors
+                loginErrorEmail: 'email är fel ifyllt',
+                loginErrorPassword: 'lösenord felaktigt ifyllt',
+                loginErrorServererror: 'Det gick inte att logga in denna användare. Fyll i på nytt och försök igen.',
+                registerErrorServererror: 'Finns redan en användare med denna email.',
+                registerErrorName: 'Namnet är fel ifyllt',
+                registerErrorImg: 'img går ej att spara',
+
+                //footer
+                authertext: 'Built and styled by: TessDevon'
+            }   
+        }
+    }
+})
